fd0b85d1ccd70e1e7b9dbb52a3e262fa30086da1
Morgan Wahl <morgan@addgene.org>
2017-04-14 15:05:17 -0400

    Multiple changes.

--- run.py
+++ run.py
@@ -1,11 +1,9 @@
-import json
+import os
 
-from slacker import Slacker
-import IPython
 
-_SECRET_TOKEN = 'xoxp-3312287274-5079590165-168774511872-d8bde62260ad21dc64c21513ef348536'
+def get_tune_from_jabber_file():
+    from lxml import objectify
 
-def get_tune():
     # <tune xmlns='http://jabber.org/protocol/tune'>
     #  <artist>%s</artist>
     #  <title>%s</title>
@@ -13,22 +11,52 @@ def get_tune():
     #  <track>%d</track>
     #  <length>%d</length>
     # </tune>
+    jabber_file = os.path.join(
+        os.environ['HOME'],
+        '.quodlibet',
+        'jabber',
+    )
+    if not os.path.exists(jabber_file):
+        return u''
+    tune = objectify.parse(jabber_file).getroot()
+    os.unlink(jabber_file)
+    return u"{}: {}".format(tune.artist, tune.title)
+
+
+def get_tune_from_quodlibet():
+    import subprocess
+    try:
+        return subprocess.check_output((
+            'quodlibet',
+            '--print-playing', '<artist>: <title>',
+        )).strip()
+    except subprocess.CalledProcessError:
+        return u''
+
 
 def set_status(text='', emoji=''):
-    slack = Slacker(_SECRET_TOKEN)
+    import json
+    from slacker import Slacker
+    token = os.environ['SLACK_SECRET_TOKEN']
+
+    slack = Slacker(token)
     # print slack.users.profile.get()
     status = json.dumps({
         'status_text': text,
         u'status_emoji': emoji,
     })
     # print status
-    # print slack.users.profile.set(profile=status)
+    slack.users.profile.set(profile=status)
+
 
 def main():
-    # Read the tune file
-    artist, title = get_tune()
-    message = u"{}: {}".format(artist, title)
-    emoji = u':musical_note:'
+    tune = get_tune_from_quodlibet()
+    if tune:
+        message = tune
+        emoji = u':musical_note:'
+    else:
+        message = u''
+        emoji = u''
     set_status(text=message, emoji=emoji)
 
 
